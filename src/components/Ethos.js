90dae64a1c57cc826b7035f05456ca5f552ec367
mark-mansfield <markgmansfield@gmail.com>
2019-05-02 13:13:05 +1000

    created html and css for ehtos component

--- src/components/Ethos.js
+++ src/components/Ethos.js
@@ -3,89 +3,27 @@ import React from 'react'
 const Ethos = () => {
   return (
     <section id="section" className="">
-      <div className="container-full-width">
-        <div className="">
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'space around',
-              height: '200px',
-              backgroundColor: '#666',
-            }}
-          >
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-                textAlign: 'center',
-                width: '200px',
-                margin: '20px',
-              }}
-            >
-              <div>
-                <i
-                  className="fas fa-code fa-2x"
-                  style={{ fontWeight: 'regular' }}
-                />
-              </div>
-
-              <p>I passionate about writing clean self documenting code</p>
-            </div>
-
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-                textAlign: 'center',
-                width: '200px',
-                margin: '20px',
-              }}
-            >
-              <div />
-
-              <i className="fas fa-universal-access fa-2x" />
-              <p>I strive to make all of my work as accessible as possible</p>
-            </div>
-
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-                textAlign: 'center',
-                width: '200px',
-                margin: '20px',
-              }}
-            >
-              <div />
-              <i className="fas fa-wrench fa-2x" />
-              <p>
-                I love my tool belt and am always looking to update my toolchain
-              </p>
-            </div>
-
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-                textAlign: 'center',
-                width: '200px',
-                margin: '20px',
-              }}
-            >
-              <div />
-              <i className="fas fa-ruler-combined fa-2x" />
-              <p>Cross browser pixel perfect layouts are no problem.</p>
-            </div>
-          </div>
-        </div>
+      <div className="container-full-width ethos">
+        <ul className="ethos-list">
+          <li>
+            <i className="fas fa-code fa-3x" />
+            <p>I passionate about writing clean self documenting code</p>
+          </li>
+          <li>
+            <i className="fas fa-universal-access fa-3x" />
+            <p>I strive to make all of my work as accessible as possible</p>
+          </li>
+          <li>
+            <i className="fas fa-wrench fa-3x" />
+            <p>
+              I love my tool belt and am always looking to update my toolchain
+            </p>
+          </li>
+          <li>
+            <i className="fas fa-ruler-combined fa-3x" />
+            <p>Cross browser pixel perfect layouts are no problem.</p>
+          </li>
+        </ul>
       </div>
     </section>
   )
