a723eb131b58677d7720e0cb7ea5b0b144eb4a3b
mark-mansfield <markgmansfield@gmail.com>
2019-05-02 11:55:13 +1000

    initial commit

---
+++ src/components/Ethos.js
@@ -0,0 +1,98 @@
+import React from 'react'
+
+const Ethos = () => {
+  return (
+    <section id="section" className="">
+      <div className="container" id="about_me">
+        <div className="container">
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space around',
+              height: '200px',
+              backgroundColor: '#666',
+            }}
+          >
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                textAlign: 'center',
+                width: '200px',
+                margin: '20px',
+              }}
+            >
+              <div>
+                <i
+                  className="fas fa-code fa-2x"
+                  style={{ fontWeight: 'regular' }}
+                />
+              </div>
+
+              <p>I passionate about writing clean self documenting code</p>
+            </div>
+
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                textAlign: 'center',
+                width: '200px',
+                margin: '20px',
+              }}
+            >
+              <div />
+
+              <i className="fas fa-universal-access fa-2x" />
+              <p>I strive to make all of my work as accessible as possible</p>
+            </div>
+
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                textAlign: 'center',
+                width: '200px',
+                margin: '20px',
+              }}
+            >
+              <div />
+              <i className="fas fa-wrench fa-2x" />
+              <p>
+                I love my tool belt and am always looking to update my toolchain
+              </p>
+            </div>
+
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                textAlign: 'center',
+                width: '200px',
+                margin: '20px',
+              }}
+            >
+              <div />
+              <i className="fas fa-ruler-combined fa-2x" />
+              <p>Cross browser pixel perfect layouts are no problem.</p>
+            </div>
+            {/* <i className="fas fa-tools" />
+              <i className="fas fa-universal-access" />
+              <i className="fas fa-code" />
+              <i className="fas fa-user" />
+              <i className="fas fa-drafting-compass" /> */}
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+export default Ethos
