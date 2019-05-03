6d86d3bcea5fcba6a9b7f19b679116374869797e
mark-mansfield <markgmansfield@gmail.com>
2019-05-03 10:56:26 +1000

    added social icons to component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,25 @@
 import React from 'react'
-
 const Contact = () => {
   return (
     <section id="section" className="contact set-height">
       <div className="container-partial-width">
         <h1>Contact Me</h1>
         <p>Contact me here</p>
+        <img
+          src={require('../images/linkedin.svg')}
+          alt="facebook link"
+          title="contact me on facebook"
+        />
+        <img
+          src={require('../images/facebook.svg')}
+          alt="facebook link"
+          title="contact me on facebook"
+        />
+        <img
+          src={require('../images/instagram.svg')}
+          alt="facebook link"
+          title="contact me on facebook"
+        />
       </div>
     </section>
   )
