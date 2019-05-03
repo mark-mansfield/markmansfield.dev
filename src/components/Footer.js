c695bb44d2619c3a7f336312819068ea090c5192
mark-mansfield <markgmansfield@gmail.com>
2019-05-03 10:38:52 +1000

    added socil icons and credit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,24 @@ import PropTypes from 'prop-types'
 const Footer = props => (
   <footer id="footer" style={props.timeout ? { display: 'none' } : {}}>
     <p className="copyright">&copy;markmansfield.dev</p>
+    <div className="copyright">
+      Icons made by &nbsp;
+      <a href="https://www.freepik.com/" title="Freepik">
+        Freepik
+      </a>
+      <br />
+      <a href="https://www.flaticon.com/" title="Flaticon">
+        www.flaticon.com
+      </a>
+      <br />
+      <a
+        href="http://creativecommons.org/licenses/by/3.0/"
+        title="Creative Commons BY 3.0"
+        target="_blank"
+      >
+        CC 3.0 BY
+      </a>
+    </div>
   </footer>
 )
 
