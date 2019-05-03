c6aeb64bf4a86942cd2c86d8f97a4431751ace77
mark-mansfield <markgmansfield@gmail.com>
2019-05-03 11:48:33 +1000

    added emial icon

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,23 +3,34 @@ const Contact = () => {
   return (
     <section id="section" className="contact set-height">
       <div className="container-partial-width">
-        <h1>Contact Me</h1>
-        <p>Contact me here</p>
-        <img
-          src={require('../images/linkedin.svg')}
-          alt="facebook link"
-          title="contact me on facebook"
-        />
-        <img
-          src={require('../images/facebook.svg')}
-          alt="facebook link"
-          title="contact me on facebook"
-        />
-        <img
-          src={require('../images/instagram.svg')}
-          alt="facebook link"
-          title="contact me on facebook"
-        />
+        <div style={{ textAlign: 'center' }}>
+          <h1>Contact Me</h1>
+          <p>You know you want to!</p>
+        </div>
+        <div className="contact-layout">
+          <img
+            className="contact-layout-item-1"
+            src={require('../images/linkedin.svg')}
+            alt="linkedin link"
+            title="contact me on linkedin"
+          />
+          <img
+            src={require('../images/gmail.svg')}
+            alt="email me here"
+            title="contact me via email"
+          />
+
+          <img
+            src={require('../images/facebook.svg')}
+            alt="facebook link"
+            title="contact me on facebook"
+          />
+          <img
+            src={require('../images/instagram.svg')}
+            alt="instagram link"
+            title="contact me on instagram"
+          />
+        </div>
       </div>
     </section>
   )
