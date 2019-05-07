7c9536dce70416e047833770bf2ff8ec9da032f6
mark-mansfield <markgmansfield@gmail.com>
2019-05-07 17:38:23 +1000

    added footer , adjusted min-height for sectiom > ethos css class

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,6 @@ import { MdClose } from 'react-icons/md'
 import Modal from 'react-modal'
 
 import Header from '../components/Header'
-// import Main from '../components/Main'
 import SayHello from '../components/SayHello'
 import About from '../components/About'
 import Ethos from '../components/Ethos'
@@ -22,28 +21,9 @@ class IndexPage extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      // isArticleVisible: false,
-      // timeout: false,
-      // articleTimeout: false,
-      // article: '',
-      // loading: 'is-loading',
       modalIsOpen: false,
     }
     this._handleShowModal = this._handleShowModal.bind(this)
-  }
-
-  componentDidMount() {
-    // this.timeoutId = setTimeout(() => {
-    //   this.setState({ loading: '' })
-    // }, 100)
-    // document.addEventListener('mousedown', this.handleClickOutside)
-  }
-
-  componentWillUnmount() {
-    // if (this.timeoutId) {
-    //   clearTimeout(this.timeoutId)
-    // }
-    // document.removeEventListener('mousedown', this.handleClickOutside)
   }
 
   _handleShowModal = () => {
@@ -60,70 +40,6 @@ class IndexPage extends React.Component {
   closeModal = () => {
     this.setState({ modalIsOpen: false })
   }
-
-  // setWrapperRef(node) {
-  //   this.wrapperRef = node
-  // }
-
-  // handleOpenArticle(article) {
-  //   this.setState({
-  //     isArticleVisible: !this.state.isArticleVisible,
-  //     article,
-  //   })
-
-  //   setTimeout(() => {
-  //     this.setState({
-  //       timeout: !this.state.timeout,
-  //     })
-  //   }, 325)
-
-  //   setTimeout(() => {
-  //     this.setState({
-  //       articleTimeout: !this.state.articleTimeout,
-  //     })
-  //   }, 350)
-  // }
-
-  // handleCloseArticle() {
-  //   this.setState({
-  //     articleTimeout: !this.state.articleTimeout,
-  //   })
-
-  //   setTimeout(() => {
-  //     this.setState({
-  //       timeout: !this.state.timeout,
-  //     })
-  //   }, 325)
-
-  //   setTimeout(() => {
-  //     this.setState({
-  //       isArticleVisible: !this.state.isArticleVisible,
-  //       article: '',
-  //     })
-  //   }, 350)
-  // }
-
-  // handleClickOutside(event) {
-  //   if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
-  //     if (this.state.isArticleVisible) {
-  //       this.handleCloseArticle()
-  //     }
-  //   }
-  // }
-
-  // static childContextTypes = {
-  //   reactIconBase: PropTypes.object,
-  // }
-
-  // getChildContext() {
-  //   return {
-  //     reactIconBase: {
-  //       fill: 'tomato',
-  //       size: 24,
-  //       style: {},
-  //     },
-  //   }
-  // }
 
   render() {
     // modal styling
@@ -154,24 +70,7 @@ class IndexPage extends React.Component {
 
     return (
       <Layout location={this.props.location}>
-        {/* <div
-          className={`body ${this.state.loading} ${
-            this.state.isArticleVisible ? 'is-article-visible' : ''
-          }`}
-        > */}
         <div id="wrapper">
-          {/* <div className="contact-layout">
-            <a
-              href="https://www.facebook.com/mmansfield"
-              alt="marks facebook page"
-              title="contact me via this platform"
-            >
-              <i className="fab fa-facebook fa-2x icon-color" />
-            </a>
-
-            <i className="fab fa-linkedin-in fa-2x icon-color" />
-            <i className="fab fa-github fa-2x icon-color" />
-          </div> */}
           <div id="hero" className="aboveFoldHeight">
             <Modal
               closeTimeoutMS={300}
@@ -208,20 +107,9 @@ class IndexPage extends React.Component {
               </section>
             </Modal>
 
-            <Header
-              // onOpenArticle={this.handleOpenArticle}
-              // timeout={this.state.timeout}
-              modalState={this._handleShowModal}
-            />
+            <Header modalState={this._handleShowModal} />
           </div>
-          {/* <Main
-              isArticleVisible={this.state.isArticleVisible}
-              timeout={this.state.timeout}
-              articleTimeout={this.state.articleTimeout}
-              article={this.state.article}
-              onCloseArticle={this.handleCloseArticle}
-              setWrapperRef={this.setWrapperRef}
-            /> */}
+
           <Skills />
           <Ethos />
           <SayHello />
@@ -229,11 +117,7 @@ class IndexPage extends React.Component {
           <About />
           <PortfolioDescription scrollTarget="portfolio" />
           <Projects />
-          {/*<
-
-
-
-            Footer timeout={this.state.timeout} /> */}
+          <Footer timeout={this.state.timeout} />
         </div>
         {/* <div id="bg" /> */}
         {/* </div> */}
