04a6943a137a3afe412ad6b3c23b00202296ad66
mark-mansfield <markgmansfield@gmail.com>
2019-06-21 11:08:31 +1000

    refactored this into a class component to use this.props.children to  get wrapped content

--- src/components/presentation-components/simple-banner/SimpleBanner.js
+++ src/components/presentation-components/simple-banner/SimpleBanner.js
@@ -1,15 +1,23 @@
-import React, { Component } from 'react'
-const SimpleBanner = props => (
-  <section
-    id="section"
-    className={props.scrollTarget + ' container-full-width'}
-  >
-    <div className="simple-banner">
-      <div className="simple-banner-text">
-        <h1>{props.title}</h1>
-        <p>{props.text}</p>
-      </div>
-    </div>
-  </section>
-)
+import React from 'react'
+class SimpleBanner extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render(props) {
+    return (
+      <section
+        id="section"
+        className={this.props.scrollTarget + ' container-full-width'}
+      >
+        <div className="simple-banner">
+          <div className="simple-banner-text">
+            <h1>{this.props.title}</h1>
+            <p>{this.props.children}</p>
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
+
 export default SimpleBanner
