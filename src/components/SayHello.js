b4085ba6183e6929d69adff19b537ad4b79e4b3c
mark-mansfield <markgmansfield@gmail.com>
2019-06-21 11:05:40 +1000

    added annimation to content

--- src/components/SayHello.js
+++ src/components/SayHello.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import SimpleBanner from '../components/presentation-components/simple-banner/SimpleBanner'
-const SayHello = () => {
-  return (
-    <SimpleBanner
-      scrollTarget="about_me"
-      title="Hi, I’m Mark. Nice to meet you."
-      text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum laudantium maxime obcaecati optio, eum placeat eaque ab unde. Beatae quibusdam, rerum ipsa voluptatum quam, totam animi aliquam doloribus consequatur ut quidem facilis dolore? Labore placeat tenetur ducimus. Nemo, animi sit.."
-    />
-  )
+import ScrollAnimation from 'react-animate-on-scroll'
+class SayHello extends React.Component {
+  render() {
+    return (
+      <SimpleBanner scrollTarget="about_me" title="" text="">
+        <ScrollAnimation animateIn="fadeInLeft" animateOnce="true" delay={400}>
+          <div style={{ display: 'grid', placeItems: 'center' }}>
+            <h1>My name is Mark, nice to meet you!</h1>
+            Lets chat about how I can fill the role you are looking for.
+            <br />
+            <br />
+            <a onClick={this.props.modalState.bind(this)}>Contact</a>
+          </div>
+        </ScrollAnimation>
+      </SimpleBanner>
+    )
+  }
 }
 export default SayHello
