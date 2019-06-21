5bbe30f29ffcf97427dc70503fe9047c069bae55
mark-mansfield <markgmansfield@gmail.com>
2019-06-21 11:07:08 +1000

    added animation to content

--- src/components/portfolio/description/Description.js
+++ src/components/portfolio/description/Description.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import SimpleBanner from '../../presentation-components/simple-banner/SimpleBanner'
+import ScrollAnimation from 'react-animate-on-scroll'
 const PortfolioDescription = props => {
   return (
-    <SimpleBanner
-      scrollTarget={props.scrollTarget}
-      title="Portfolio"
-      text="Below you can see some projects I've been working on lately. As I am
-    starting out all of these projects are either non paid or fictitious
-    projects."
-    />
+    <SimpleBanner scrollTarget={props.scrollTarget} title="Portfolio" text="">
+      <ScrollAnimation animateIn="fadeInLeft" animateOnce="true" delay={400}>
+        Below you can see some projects I've been working on lately. As I am
+        starting out all of these projects are either non paid or fictitious
+        projects.
+      </ScrollAnimation>
+    </SimpleBanner>
   )
 }
 export default PortfolioDescription
