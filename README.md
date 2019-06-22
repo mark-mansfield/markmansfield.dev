fbd0da8edcc097b59a5b45c753c78050175d0306
mark-mansfield <markgmansfield@gmail.com>
2019-06-23 09:13:23 +1000

    removed or relocated image

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 ## My Web developer portfolio site
 
-
-![](icon-gatsby-48x48.png) ![](icon-react-48x48.png)![](icon-webpack-small.png)
-
-## Fonts
-![](icon-font-awesome-48x48.png) v5
+A custom portfolio website for mark mansfield built using gatsby.
--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/icon-gatsby-48x48', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-sass',
